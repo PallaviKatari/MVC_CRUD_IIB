e0dd916ce8ac6515d40cd55e949234e084dd2eaa
PallaviKatari <pallavikatari@gmail.com>
2025-11-28 16:33:50 +0530

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Demo.Models;
+using MVC_Demo.Services;
+
+namespace MVC_Demo.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly CustomerService _service;
+        public CustomerController(CustomerService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index(int? id, string? actionType)
+        {
+            ViewBag.ActionType = actionType;
+            if (id.HasValue)
+            {
+                var customer = _service.GetById(id.Value);
+                return View(customer);
+            }
+            var customers = _service.GetAll();
+            return View(customers);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.Add(customer);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.Update(customer);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _service.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult BackToList()
+        {
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Services/CustomerService.cs
@@ -0,0 +1,37 @@
+﻿using MVC_Demo.Models;
+using System.Xml.Linq;
+
+namespace MVC_Demo.Services
+{
+    //Service for In-Memory List
+    public class CustomerService
+    {
+        private readonly List<Customer> _customers = new();
+
+        public List<Customer> GetAll() => _customers;
+
+        public Customer? GetById(int id) => _customers.FirstOrDefault(c => c.Id == id);
+
+        public void Add(Customer customer)
+        {
+            customer.Id = _customers.Count > 0 ? _customers.Max(c => c.Id) + 1 : 1;
+            _customers.Add(customer);
+        }
+
+        public void Update(Customer customer)
+        {
+            var existing = GetById(customer.Id);
+            if (existing != null)
+            {
+                existing.Name = customer.Name;
+                existing.Email = customer.Email;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var existing = GetById(id);
+            if (existing != null) _customers.Remove(existing);
+        }
+    }
+}
